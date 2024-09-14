7c7b13e99036f5ef1460f1e481bf541402111fd7
btp5 btp5 <btp5@roboxaservices.com>
2024-09-14 06:50:49 +0000

    final

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -83,6 +83,20 @@ sap.ui.define([
                 oFrag.setBindingContext(oContext);
                 oFrag.open();})
         },
+        _onAddTab1(oEvent,path){
+            var sPath=oEvent.getSource().getBindingContext().getPath();
+            var tablePath=oEvent.getSource().getParent().getParent().getTableBindingPath();
+            var oBj=oEvent.getSource().getBindingContext().getObject();
+            var oContext=this._owner.createEntry(`${sPath}/${tablePath}`,{
+                groupId:"create",
+                properties:{
+                    Sitecode:oBj.SiteCode
+                }
+            });
+            this._openFragment(path).then((oFrag)=>{
+                oFrag.setBindingContext(oContext);
+                oFrag.open();})
+        },
         _onClose(oEvent){
             this._owner.resetChanges()
             oEvent.getSource().getParent().getParent().close();
@@ -114,6 +128,60 @@ sap.ui.define([
             this._onClose(oEvent);
         },
         onSave2(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab3(oEvent){
+            this._onAddTab(oEvent,"rbx.107.clinicalresearcher.fragments.licenseAdd")
+        },
+        onClose3(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave3(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab4(oEvent){
+            this._onAddTab(oEvent,"rbx.107.clinicalresearcher.fragments.enseAdd")
+        },
+        onClose4(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave4(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab5(oEvent){
+            this._onAddTab(oEvent,"rbx.107.clinicalresearcher.fragments.gcpAdd")
+        },
+        onClose5(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave5(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab6(oEvent){
+            this._onAddTab(oEvent,"rbx.107.clinicalresearcher.fragments.spciaAdd")
+        },
+        onClose6(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave6(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab7(oEvent){
+            this._onAddTab1(oEvent,"rbx.107.clinicalresearcher.fragments.totalAdd")
+        },
+        onClose7(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave7(oEvent){
+            this._onSubmit(oEvent);
+        },
+        onAddTab8(oEvent){
+            this._onAddTab1(oEvent,"rbx.107.clinicalresearcher.fragments.regulatoryAdd")
+        },
+        onClose8(oEvent){
+            this._onClose(oEvent);
+        },
+        onSave8(oEvent){
             this._onSubmit(oEvent);
         }
     });
